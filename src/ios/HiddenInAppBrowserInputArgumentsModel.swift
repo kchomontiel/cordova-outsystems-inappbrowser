6e91665f365f1a9b22ff8e5cbfd6c7bfe6d4771d
Carlos <kchomontiei@gmail.com>
2025-09-02 11:43:07 -0300

    Mas fix de compilacion iOS

--- src/ios/HiddenInAppBrowserInputArgumentsModel.swift
+++ src/ios/HiddenInAppBrowserInputArgumentsModel.swift
@@ -1,7 +1,16 @@
 import Foundation
 
-class HiddenInAppBrowserInputArgumentsSimpleModel {
+struct HiddenInAppBrowserInputArgumentsSimpleModel: Decodable {
     let url: String
+    
+    enum CodingKeys: String, CodingKey {
+        case url
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        url = try container.decode(String.self, forKey: .url)
+    }
     
     init(url: String) {
         self.url = url
