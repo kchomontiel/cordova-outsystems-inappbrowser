22ff9a49528443d4f2d386723444d0b88379958a
Carlos <kchomontiei@gmail.com>
2025-09-03 12:21:50 -0300

    ✅ Exposición global en window.multibrowser ✅ Exposición en Cordova en cordova.plugins.multibrowser ✅ Método closeWebView agregado ✅ Compatibilidad con diferentes entornos

--- plugins/multibrowser/dist/plugin.js
+++ plugins/multibrowser/dist/plugin.js
@@ -1,17 +1,60 @@
-var exec = require('cordova/exec');
+var exec = require("cordova/exec");
 
 var multibrowser = {
-    openInWebView: function(url, target, options, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'multibrowser', 'openInWebView', [url, target, options]);
-    },
-    
-    openHidden: function(url, target, options, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'multibrowser', 'openHidden', [url, target, options]);
-    },
-    
-    openInExternalBrowser: function(url, target, options, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'multibrowser', 'openInExternalBrowser', [url, target, options]);
-    }
+  openInWebView: function (
+    url,
+    target,
+    options,
+    successCallback,
+    errorCallback
+  ) {
+    exec(successCallback, errorCallback, "multibrowser", "openInWebView", [
+      url,
+      target,
+      options,
+    ]);
+  },
+
+  openHidden: function (url, target, options, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "multibrowser", "openHidden", [
+      url,
+      target,
+      options,
+    ]);
+  },
+
+  openInExternalBrowser: function (
+    url,
+    target,
+    options,
+    successCallback,
+    errorCallback
+  ) {
+    exec(
+      successCallback,
+      errorCallback,
+      "multibrowser",
+      "openInExternalBrowser",
+      [url, target, options]
+    );
+  },
+
+  closeWebView: function (successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "multibrowser", "closeWebView", []);
+  },
 };
 
-module.exports = multibrowser;
+// Exponer globalmente para Cordova
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = multibrowser;
+}
+
+// Exponer en window para uso global
+if (typeof window !== "undefined") {
+  window.multibrowser = multibrowser;
+}
+
+// Exponer en cordova.plugins
+if (typeof cordova !== "undefined" && cordova.plugins) {
+  cordova.plugins.multibrowser = multibrowser;
+}
